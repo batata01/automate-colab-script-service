b2a37c9e2e8a2ebee97f000be3244e18a86a3b11
batata01 <94710515+batata01@users.noreply.github.com>
2022-01-13 17:11:12 +0100

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-MOZ_HEADLESS=1 /usr/bin/python /home/asgaralipq/Work/Colab-Script/script.py
-bash /home/asgaralipq/Work/Colab-Script/move.sh
+MOZ_HEADLESS=1 /usr/bin/python /root/work/colabscript/script.py
+bash /root/work/colabscript/move.sh
 killall -q -9 firefox
